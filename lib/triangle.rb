b7997cd2dac12a60a3c2b3854adf40fef9782678
Nolan Byrnes <nolanehs@gmail.com>
2017-11-28 12:59:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,13 @@
 class Triangle
   # write code here
 attr_accessor :a, :b, :c
-  
-def initialize(:a, :b, :c)
+
+def initialize(a, b, c)
 
 end
 
 
-  
+
 end
 
 class TriangleError < StandardError
