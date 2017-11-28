88488754963334ca86e4196cec381372eca45917
Nolan Byrnes <nolanehs@gmail.com>
2017-11-28 13:08:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,24 @@
 class Triangle
   # write code here
-attr_accessor :equilateral, :isosceles, :scalene
+attr_accessor :a, :b, :c
 
 def initialize(a, b, c)
-
+@a = a
+@b = b
+@c = c
 end
 
+def kind
+valid_triangle
+if a==b && b==c
+  :equilateral
+elsif a==b || b==c || a==c
+  :isosceles
+else
+  :scalene
+end
+
+end
 
 
 end
